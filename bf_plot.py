deda0786f95485b07b328fd8210da0316ab1b4bc
jinyeom <jinyeom95@gmail.com>
2018-05-02 00:14:11 -0500

    progress

--- bf_plot.py
+++ bf_plot.py
@@ -1,9 +1,18 @@
 import re
+import os
 import subprocess
 import numpy as np
 from matplotlib import pyplot as plt
 
-def plot_nthreads_time(prog, filename, src, title, show=False, save=False):
+def omp_schedule_plot(prog, filename, src, title, save):
+    clause = ["static", "dynamic"]
+    chunk_sizes = [1, 8, 32, 128, 512]
+    for c in clause:
+        for s in chunk_sizes:
+            os.environ["OMP_SCHEDULE"] = "{},{}".format(c, s)
+            plot_nthreads_time(prog, filename, src, title, c, s, save)
+
+def plot_nthreads_time(prog, filename, src, title, clause, chunk_size, save):
     # plot the naive pthread implemenation of computation of pi with 1, 2, 4, 8 threads
     print("Plotting runtimes and speedups of {} with 1, 2, 4, 8 threads...".format(prog))
     runtimes = []
@@ -11,17 +20,23 @@ def plot_nthreads_time(prog, filename, src, title, show=False, save=False):
 
     # first get the runtime of serial implementation
     print("Running {} on {} in serial...".format(prog, filename), flush=True, end="")
-    result = subprocess.run([prog, "-f", filename, "-s", src], stdout=subprocess.PIPE).stdout.decode("utf-8")
+    serial_runtime = 0.0
+    for i in range(5):
+        result = subprocess.run([prog, "-f", filename, "-s", src], stdout=subprocess.PIPE).stdout.decode("utf-8")
+        serial_runtime += float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
+    serial_runtime = serial_runtime / 5.0 # get average time
     print("\x1B[32mdone\x1B[0m")
-    serial_runtime = float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
 
     n_threads = [1, 2, 4, 8]
     for n in n_threads:
         print("Running {} on {} with {} thread(s)...".format(prog, filename, n), flush=True, end="")
-        result = subprocess.run([prog, "-f", filename, "-s", src, "-p", str(n)], stdout=subprocess.PIPE).stdout.decode("utf-8")
+        parallel_runtime = 0.0
+        for i in range(5):
+            result = subprocess.run([prog, "-f", filename, "-s", src, "-p", str(n)], stdout=subprocess.PIPE).stdout.decode("utf-8")
+            parallel_runtime += float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
+        parallel_runtime = parallel_runtime / 5.0 # get average time
         print("\x1B[32mdone\x1B[0m")
-        # first line of the result should have the runtime
-        parallel_runtime = float(re.search(r"elapsed process CPU time = (.*) nanoseconds", result).group(1))
+        
         runtimes.append(parallel_runtime)
         speedups.append(serial_runtime / parallel_runtime)
 
@@ -33,12 +48,9 @@ def plot_nthreads_time(prog, filename, src, title, show=False, save=False):
     plt.axhline(y=serial_runtime, color="r", label="serial")
     plt.plot(n_threads, runtimes, marker="o")
     plt.legend()
-    if show:
-        print("Displaying the plot...")
-        plt.show()
     if save:
         print("Saving plot figure...", flush=True, end="")
-        plt.savefig("{}_runtime.png".format(title))
+        plt.savefig("{}_runtime_({},{}).png".format(title, clause, chunk_size))
         print("\x1B[32mdone\x1B[0m")
         plt.close()
    
@@ -48,16 +60,13 @@ def plot_nthreads_time(prog, filename, src, title, show=False, save=False):
     plt.xlabel("number of threads")
     plt.ylabel("speedup")
     plt.plot(n_threads, speedups, marker="o")
-    if show:
-        print("Displaying the plot...")
-        plt.show()
     if save:
         print("Saving plot figure...", flush=True, end="")
-        plt.savefig("{}_speedup.png".format(title))
+        plt.savefig("{}_speedup_({},{}).png".format(title, clause, chunk_size))
         print("\x1B[32mdone\x1B[0m")
         plt.close()
 
-plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/rmat15.dimacs", "1", "rmat15", save=True)
-plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/rmat22.dimacs", "1", "rmat22", save=True)
-plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/roadNY.dimacs", "140961", "roadNY", save=True)
-plot_nthreads_time("./bellman-ford", "/tmp/jinyeom/roadFLA.dimacs", "316607", "roadFLA", save=True)
+omp_schedule_plot("./bellman-ford", "/tmp/jinyeom/rmat15.dimacs", "1", "rmat15", True)
+omp_schedule_plot("./bellman-ford", "/tmp/jinyeom/rmat22.dimacs", "1", "rmat22", True)
+omp_schedule_plot("./bellman-ford", "/tmp/jinyeom/roadNY.dimacs", "140961", "roadNY", True)
+omp_schedule_plot("./bellman-ford", "/tmp/jinyeom/roadFLA.dimacs", "316607", "roadFLA", True)
--- src/main.cpp
+++ src/main.cpp
@@ -11,45 +11,20 @@
 
 #include "../include/graph.h"
 
-#define NUM_TRIALS 5
-#define BUFF_SIZE 100000
 #define INF std::numeric_limits<int>::max()
 
 // NOTE: initialize the graph in main
 graph g;
 
-// NOTE: declare the global array with a buffer; if the argument graph has more nodes than the
-// buffer size, reallocate the array with the argument number of nodes.
-std::atomic<graph::edge_data_t>* distance = new std::atomic<graph::edge_data_t>[BUFF_SIZE];
-
 void Usage(char *prog) {
     std::cout << "usage: " << prog << " -f filename -s src_id [-p num_threads]" << std::endl;
 }
 
-// update distance between two nodes given the current weight using a CAS operation
-void UpdateDistance(const graph::node_t& u, const graph::node_t& v, const graph::edge_data_t& w) {
-    bool done = false;
-    while (!done) {
-        auto dist_old = distance[v].load();
-        auto distu = distance[u].load();
-        auto dist_new = distu + w;
-        if (distu != INF && dist_new < dist_old) {
-            done = distance[v].compare_exchange_weak(dist_old, dist_new);
-        } else {
-            done = true;
-        }
-    }
-}
-
 int main(int argc, char* argv[]) {
-    // __itt_pause();
-
     std::string filename;               // graph file name
     graph::node_t src;                  // source node ID
     bool parallel = false;              // run in parallel if true
     int num_threads = 0;                // number of threads
-    std::string clause;                 // schedule clause (either static or dynamic)
-    int chunk_size = 0;                 // chunk size for scheduling
 
     std::ofstream f;                    // result text file
     std::ostringstream result_filename; // result text file name
@@ -64,11 +39,9 @@ int main(int argc, char* argv[]) {
         } else if (!strcmp(argv[i], "-p")) {
             parallel = true;
             num_threads = std::stoi(argv[++i]);
-            clause = argv[++i];
-            chunk_size = std::stoi(argv[++i]);
         }
     }
-    if (filename.empty() || (parallel && num_threads == 0 && clause.empty() && chunk_size == 0)) {
+    if (filename.empty() || (parallel && num_threads == 0)) {
         Usage(argv[0]);
         return -1;
     }
@@ -78,10 +51,8 @@ int main(int argc, char* argv[]) {
 
     // initialize the graph
     int num_nodes = (int)g.size_nodes();
-    if (num_nodes > BUFF_SIZE) {
-        delete [] distance;
-        distance = new std::atomic<graph::edge_data_t>[num_nodes];
-    }
+    std::atomic<graph::edge_data_t>* distance;
+    distance = new std::atomic<graph::edge_data_t>[num_nodes];
     for (int i = 0; i < num_nodes; ++i) {
         distance[i] = INF;
     }
@@ -89,81 +60,57 @@ int main(int argc, char* argv[]) {
 
     if (parallel) {
         // OpenMP parallel implementaion
-        std::cout << "solving SSSP from node " << (int)src << " via parallel (" << num_threads << ") Bellman-Ford algorithm..." << std::endl;
+        std::cout << "Solving SSSP from node " << (int)src << " via parallel Bellman-Ford algorithm..." << std::endl;
 
         double execTime = 0.0; // time in nanoseconds
-        double tick, tock; // in seconds
+        double tick = 0.0; // in seconds
+        double tock = 0.0;
 
         // set the number of threads for OpenMP
         omp_set_num_threads(num_threads);
 
-        if (clause == "static") {
-            for (int t = 0; t < NUM_TRIALS; ++t) {
-                #pragma omp parallel
-                {
-                    #pragma omp master
-                    {
-                        int nthreads = omp_get_num_threads();
-                        if (nthreads != num_threads) {
-                            std::cout << "incorrect number of threads: " << nthreads << " != " << num_threads << std::endl;
-                        }
-                        tick = omp_get_wtime();
-                    }
-                    // ---------------- experiment below ----------------
+        #pragma omp parallel
+        {
+            #pragma omp master
+            {
+                int nthreads = omp_get_num_threads(); // actual number of threads
+                std::cout << "OpenMP: running " << argv[0] << " with " << nthreads << " threads" << std::endl;
+                tick = omp_get_wtime();
+            }
+            // ---------------- experiment below ----------------
 
-                    #pragma omp for schedule(static, chunk_size)
-                        for (graph::node_t u = g.begin(); u < g.end(); ++u) {
-                            graph::edge_data_t dist = distance[u];
-                            for (graph::edge_t e = g.edge_begin(u); e < g.edge_end(u); ++e) {
-                                graph::node_t v = g.get_edge_dst(e);
-                                graph::edge_data_t w = g.get_edge_data(e);
-                                UpdateDistance(u, v, w);
+	    for (int i = 0; i < num_nodes - 1; ++i) {
+                #pragma omp for schedule(runtime)
+                for (graph::node_t u = g.begin(); u < g.end(); u++) {
+                    graph::edge_data_t dist = distance[u];
+                    for (graph::edge_t e = g.edge_begin(u); e < g.edge_end(u); e++) {
+                        graph::node_t v = g.get_edge_dst(e);
+                        graph::edge_data_t w = g.get_edge_data(e);
+                        graph::edge_data_t updated = dist + w;
+                        
+                        bool done = false;
+                        while (!done) {
+                            auto dist_old = distance[v].load();
+                            auto distu = distance[u].load();
+                            auto dist_new = distu + w;
+                            if (distu != INF && dist_new < dist_old) {
+                                done = distance[v].compare_exchange_weak(dist_old, dist_new);
+                            } else {
+                                done = true;
                             }
                         }
-
-                    // --------------------------------------------------
-                    #pragma omp master
-                    {
-                        tock = omp_get_wtime();
-                        execTime += 1000000000.0 * (tock - tick);
                     }
                 }
-            }
-        } else if (clause == "dynamic") {
-            for (int t = 0; t < NUM_TRIALS; ++t) {
-                #pragma omp parallel
-                {
-                    #pragma omp master
-                    {
-                        int nthreads = omp_get_num_threads();
-                        if (nthreads != num_threads) {
-                            std::cout << "incorrect number of threads: " << nthreads << " != " << num_threads << std::endl;
-                        }
-                        tick = omp_get_wtime();
-                    }
-                    // ---------------- experiment below ----------------
+	    }
 
-                    #pragma omp for schedule(dynamic, chunk_size)
-                        for (graph::node_t u = g.begin(); u < g.end(); ++u) {
-                            graph::edge_data_t dist = distance[u];
-                            for (graph::edge_t e = g.edge_begin(u); e < g.edge_end(u); ++e) {
-                                graph::node_t v = g.get_edge_dst(e);
-                                graph::edge_data_t w = g.get_edge_data(e);
-                                UpdateDistance(u, v, w);
-                            }
-                        }
-
-                    // --------------------------------------------------
-                    #pragma omp master
-                    {
-                        tock = omp_get_wtime();
-                        execTime += 1000000000.0 * (tock - tick);
-                    }
-                }
-            }
+            // --------------------------------------------------
+            #pragma omp master
+	    {
+	        tock = omp_get_wtime();
+                execTime = 1000000000.0 * (tock - tick);
+	    }
         }
-        execTime = execTime / (double)NUM_TRIALS;
-        std::cout << "elapsed process CPU time = " << execTime << " nanoseconds\n";
+        std::cout << "elapsed process CPU time = " << (long long unsigned int)execTime << " nanoseconds\n";
         result_filename << "parallel_" << filename << ".txt";
 
         // export result to a text file
@@ -182,26 +129,39 @@ int main(int argc, char* argv[]) {
         double execTime = 0.0; // time in nanoseconds
         struct timespec tick, tock;
 
-        for (int t = 0; t < NUM_TRIALS; ++t) {
-            // main computation for SSSP; measure runtime here
-            // NOTE: probably should flush cache every run here...
-            clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
-            // ---------------- experiment below ----------------
+        // main computation for SSSP; measure runtime here
+        // NOTE: probably should flush cache every run here...
+        clock_gettime(CLOCK_MONOTONIC_RAW, &tick);
+        // ---------------- experiment below ----------------
 
-            for (graph::node_t u = g.begin(); u < g.end(); ++u) {
+	// there needs to be at most |V| - 1 relaxation until convergence;
+        // this part of the algorithm cannot be parallelized
+	for (int i = 0; i < num_nodes - 1; ++i) {
+            for (graph::node_t u = g.begin(); u < g.end(); u++) {
                 graph::edge_data_t dist = distance[u];
-                for (graph::edge_t e = g.edge_begin(u); e < g.edge_end(u); ++e) {
+                for (graph::edge_t e = g.edge_begin(u); e < g.edge_end(u); e++) {
                     graph::node_t v = g.get_edge_dst(e);
                     graph::edge_data_t w = g.get_edge_data(e);
-                    UpdateDistance(u, v, w);
+                    graph::edge_data_t updated = dist + w;
+                    
+		    bool done = false;
+                    while (!done) {
+                        auto dist_old = distance[v].load();
+                        auto distu = distance[u].load();
+                        auto dist_new = distu + w;
+                        if (distu != INF && dist_new < dist_old) {
+                            done = distance[v].compare_exchange_weak(dist_old, dist_new);
+                        } else {
+                            done = true;
+                        }
+                    }
                 }
             }
+	}
 
-            // --------------------------------------------------
-            clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
-            execTime += 1000000000.0 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
-        }
-        execTime = execTime / (double)NUM_TRIALS;
+        // --------------------------------------------------
+        clock_gettime(CLOCK_MONOTONIC_RAW, &tock);
+        execTime = 1000000000.0 * (tock.tv_sec - tick.tv_sec) + tock.tv_nsec - tick.tv_nsec;
         std::cout << "elapsed process CPU time = " << (long long unsigned int)execTime << " nanoseconds\n";
         result_filename << filename << ".txt";
 
